214a11a9bd046f8c930f9527d0a5b153b9df5702
isabellecwright <isabellewright@yahoo.com>
2012-12-18 18:33:08 -0500

    Imported lift code

--- src/org/team2168/subsystems/Lift.java
+++ src/org/team2168/subsystems/Lift.java
@@ -1,12 +1,65 @@
 package org.team2168.subsystems;
 
+import org.team2168.RobotMap;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.AnalogChannel;
+	//WASH THE DISHES
+	//LAVAR LOS PLATOS
 
 public class Lift extends Subsystem {
+	double voltage;
+	Victor liftVictor1 = new Victor(RobotMap.lift1Victor);
+	Victor liftVictor2 = new Victor(RobotMap.lift2Victor);
+	DoubleSolenoid backFlapSolenoid = new DoubleSolenoid(RobotMap.backFlapSolenoidOpen, RobotMap.backFlapSolenoidClose);
+	AnalogChannel anlgChnlUpper = new AnalogChannel(RobotMap.ballDetectorUpper);
+	
+	public Lift () {
+		
+	}
+	
+	public Lift(double voltage) {
+		this.voltage = voltage;
+	}
 
+	public boolean ballPresent() {
+		boolean ret = false;
+		double anlgChnlVoltage = anlgChnlUpper.getVoltage();
+		if (anlgChnlVoltage <= RobotMap.ballPresentVoltage) {
+			ret = true;
+		}
+		return ret;
+	}
+
+	
 	protected void initDefaultCommand() {
-		// TODO Auto-generated method stub
 
+		
+	}
+	
+	public void liftUp() {
+		liftVictor1.set(RobotMap.liftVoltage);
+		liftVictor2.set(RobotMap.liftVoltage);
+	}
+	
+	public void liftDown() {
+		liftVictor1.set(-RobotMap.liftVoltage);
+		liftVictor2.set(-RobotMap.liftVoltage);
+	}
+	
+	public void stopLift() {
+		liftVictor1.stopMotor();
+		liftVictor2.stopMotor();
+	}
+	//¿COMO ESTAS?
+	//THIS IS THE HOPPER LOLOLLLLLLL
+	public void openBackFlap() {
+		backFlapSolenoid.set(DoubleSolenoid.Value.kReverse);
+	}
+	
+	public void closeBackFlap() {
+		backFlapSolenoid.set(DoubleSolenoid.Value.kForward);
 	}
 
 }
