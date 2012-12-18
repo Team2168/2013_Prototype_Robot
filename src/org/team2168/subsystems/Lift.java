6ca5cb0411ba10923dc7880af75020f460871a59
isabellecwright <isabellewright@yahoo.com>
2012-12-18 18:32:07 -0500

    Created class

---
+++ src/org/team2168/subsystems/Lift.java
@@ -0,0 +1,12 @@
+package org.team2168.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Lift extends Subsystem {
+
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
