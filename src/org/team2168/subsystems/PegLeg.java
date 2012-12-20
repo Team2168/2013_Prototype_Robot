150d3b42b4995ebe081afc204a2d7f3313c0ae6a
James <james@team2168.org>
2012-12-19 19:28:56 -0500

    Style Change

--- src/org/team2168/subsystems/PegLeg.java
+++ src/org/team2168/subsystems/PegLeg.java
@@ -7,26 +7,25 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class PegLeg extends Subsystem {
 	Relay pegLeg;
+	
+	public PegLeg() {
+		pegLeg = new Relay(RobotMap.PegLegRelay); //makes the pegleg
+														
+	}
+	protected void initDefaulyCommand() {
+	
+	}
+	
+	public void pegLegDown(){
+		pegLeg.set(Relay.Value.kOn);
+	}
+	
+	public void pegLegUp(){
+		pegLeg.set(Relay.Value.kOff);
+	}
+	
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
 		
-		public PegLeg() {
-			pegLeg = new Relay(RobotMap.PegLegRelay); //makes the pegleg
-															
-		}
-		protected void initDefaulyCommand() {
-		
-			
-		}
-		
-		public void pegLegDown(){
-			pegLeg.set(Relay.Value.kOn);
-		}
-		
-		public void pegLegUp(){
-			pegLeg.set(Relay.Value.kOff);
-		}
-		
-		protected void initDefaultCommand() {
-			// TODO Auto-generated method stub
-			
-		}
+	}
 }
