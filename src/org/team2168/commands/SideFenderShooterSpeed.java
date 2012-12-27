b02f047de531be2e0cfdaa0b77f2c01893abdc8b
James <james@team2168.org>
2012-12-27 16:22:00 -0500

    Style Change

--- src/org/team2168/commands/SideFenderShooterSpeed.java
+++ src/org/team2168/commands/SideFenderShooterSpeed.java
@@ -11,27 +11,22 @@ public class SideFenderShooterSpeed extends CommandBase {
 	}
 	
 	protected void initialize() {
-		// TODO Auto-generated method stub
 
 	}
 
 	protected void execute() {
-		// TODO Auto-generated method stub
 		shooter.setPoint(RobotMap.HIGH_GOAL_SIDE);
 	}
 
 	protected boolean isFinished() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	protected void end() {
-		// TODO Auto-generated method stub
 
 	}
 
 	protected void interrupted() {
-		// TODO Auto-generated method stub
 
 	}
 
