3e20ed73891f21c2daf22e9528b6980e365d68f9
James <james@team2168.org>
2012-12-27 16:18:04 -0500

    Cleaned up comments.

--- src/org/team2168/commands/HoodUp.java
+++ src/org/team2168/commands/HoodUp.java
@@ -9,27 +9,22 @@ public class HoodUp extends CommandBase {
 	}
 
 	protected void initialize() {
-		// TODO Auto-generated method stub
 
 	}
 
 	protected void execute() {
-		// TODO Auto-generated method stub
 		shooter.hoodUp();
 	}
 
 	protected boolean isFinished() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	protected void end() {
-		// TODO Auto-generated method stub
 
 	}
 
 	protected void interrupted() {
-		// TODO Auto-generated method stub
 
 	}
 
