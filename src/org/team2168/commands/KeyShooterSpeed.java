73381ce8a816516bfde2f87cd4d4a6efe73aa246
James <james@team2168.org>
2012-12-27 16:19:06 -0500

    Style Change

--- src/org/team2168/commands/KeyShooterSpeed.java
+++ src/org/team2168/commands/KeyShooterSpeed.java
@@ -10,27 +10,22 @@ public class KeyShooterSpeed extends CommandBase {
 		requires(shooter);
 	}
 	protected void initialize() {
-		// TODO Auto-generated method stub
 
 	}
 
 	protected void execute() {
-		// TODO Auto-generated method stub
 		shooter.setPoint(RobotMap.HIGH_GOAL_KEY);
 	}
 
 	protected boolean isFinished() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	protected void end() {
-		// TODO Auto-generated method stub
 
 	}
 
 	protected void interrupted() {
-		// TODO Auto-generated method stub
 
 	}
 
