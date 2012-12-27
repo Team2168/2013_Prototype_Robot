4b5432da984fe6c70ce954d1c4b1c4c93801b820
James <james@team2168.org>
2012-12-27 16:19:46 -0500

    Style Change

--- src/org/team2168/commands/FenderShooterSpeed.java
+++ src/org/team2168/commands/FenderShooterSpeed.java
@@ -11,27 +11,22 @@ public class FenderShooterSpeed extends CommandBase {
 	}
 
 	protected void initialize() {
-		// TODO Auto-generated method stub
 
 	}
 
 	protected void execute() {
-		// TODO Auto-generated method stub
 		shooter.setPoint(RobotMap.HIGH_GOAL_FORWARD);
 	}
 
 	protected boolean isFinished() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	protected void end() {
-		// TODO Auto-generated method stub
 
 	}
 
 	protected void interrupted() {
-		// TODO Auto-generated method stub
 
 	}
 
